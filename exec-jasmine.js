66160e143222942d751dbc70ff0053e927118a92
Ingvar Chuniak <i.chunyak@gmail.com>
2021-06-13 20:36:53 +0300

    Prettify all the files

--- exec-jasmine.js
+++ exec-jasmine.js
@@ -2,6 +2,6 @@ const Jasmine = require("jasmine");
 
 const jasmine = new Jasmine();
 
-jasmine.loadConfigFile('./spec/support/jasmine.json');
+jasmine.loadConfigFile("./spec/support/jasmine.json");
 
 jasmine.execute();
